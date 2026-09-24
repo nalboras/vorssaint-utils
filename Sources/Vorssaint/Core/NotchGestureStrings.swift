9d79127171271813bf6abf43f24ebd7e4cc6221f
nalboras <79794909+nalboras@users.noreply.github.com>
2026-09-24 10:57:39 +0200

    Add Polish language fallback

--- Sources/Vorssaint/Core/NotchGestureStrings.swift
+++ Sources/Vorssaint/Core/NotchGestureStrings.swift
@@ -60,6 +60,7 @@ extension FeatureStrings {
             title: "Dynamic Island手勢",
             description: "捲動以打開或關閉Dynamic Island，左右滑動以切換歌曲。",
             hint: "向下捲動以打開，在頂部列向上捲動以關閉。在音樂上左右滑動以切換歌曲。列表保留原有的捲動方式。")
+        case .pl: return notchGestures(.enUS)
         case .zhHK: return NotchGestureStrings(
             title: "Dynamic Island手勢",
             description: "捲動以開啟或關閉Dynamic Island，左右滑動以切換歌曲。",
