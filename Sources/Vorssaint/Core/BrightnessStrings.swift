9935b1f9360ee63599f47330c6a3d9fb065c1a50
nalboras <79794909+nalboras@users.noreply.github.com>
2026-09-24 10:59:04 +0200

    Add Polish language fallback

--- Sources/Vorssaint/Core/BrightnessStrings.swift
+++ Sources/Vorssaint/Core/BrightnessStrings.swift
@@ -51,6 +51,7 @@ extension FeatureStrings {
         case .ko: return .ko
         case .zhHans: return .zhHans
         case .zhTW: return .zhTW
+        case .pl: return .enUS
         case .zhHK: return .zhHK
         }
     }
