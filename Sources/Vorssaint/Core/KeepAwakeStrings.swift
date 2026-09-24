9d125cc7d15b878538730fc0c70016062a470533
nalboras <79794909+nalboras@users.noreply.github.com>
2026-09-24 10:58:02 +0200

    Add Polish language fallback

--- Sources/Vorssaint/Core/KeepAwakeStrings.swift
+++ Sources/Vorssaint/Core/KeepAwakeStrings.swift
@@ -59,6 +59,7 @@ extension FeatureStrings {
         case .ko: return .ko
         case .zhHans: return .zhHans
         case .zhTW: return .zhTW
+        case .pl: return .enUS
         case .zhHK: return .zhHK
         }
     }
@@ -77,6 +78,7 @@ extension FeatureStrings {
         case .ko: return .ko
         case .zhHans: return .zhHans
         case .zhTW: return .zhTW
+        case .pl: return .enUS
         case .zhHK: return .zhHK
         }
     }
