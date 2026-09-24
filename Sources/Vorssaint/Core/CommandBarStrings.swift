41c154dd7cb8b79cd43deeeaec8303112e0d2d5f
nalboras <79794909+nalboras@users.noreply.github.com>
2026-09-24 10:59:52 +0200

    Add Polish language fallback

--- Sources/Vorssaint/Core/CommandBarStrings.swift
+++ Sources/Vorssaint/Core/CommandBarStrings.swift
@@ -189,6 +189,7 @@ extension FeatureStrings {
         case .ko: return .ko
         case .zhHans: return .zhHans
         case .zhTW: return .zhTW
+        case .pl: return .enUS
         case .zhHK: return .zhHK
         }
     }
